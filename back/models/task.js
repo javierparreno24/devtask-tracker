a73dba46e92d68d9f2d55cba2c398cdfb7f1ebe6
Javier.Parreño <javierparreno.24@campuscamara.es>
2026-01-22 13:45:14 +0100

    implementación task.js

---
+++ back/models/task.js
@@ -0,0 +1,25 @@
+// backend/models/Task.js
+const mongoose = require('mongoose');
+
+// Definimos el esquema (el plano de arquitectura de tu dato)
+const TaskSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true // Obligatorio
+    },
+    tecnologia: {
+        type: String,
+        required: true 
+    },
+    estado: {
+        type: String,
+        enum: ['pending', 'done'], // Limitamos los valores permitidos
+        default: 'pending' // Por defecto está pendiente
+    },
+    fecha: {
+        type: Date,
+        default: Date.now // Se pone la fecha actual automáticamente
+    }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
